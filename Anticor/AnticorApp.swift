d20132b25857900dff139e580bf4bf8fa5516ef9
applebro <arab1997>
2021-12-14 12:49:13 +0500

    Initial Commit

---
+++ Anticor/AnticorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnticorApp.swift
+//  Anticor
+//
+//  Created by Apple Bro on 14/12/21.
+//
+
+import SwiftUI
+
+@main
+struct AnticorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Anticor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Anticor
+//
+//  Created by Apple Bro on 14/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
